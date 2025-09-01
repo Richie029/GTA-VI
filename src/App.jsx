cada7e99413bec3baffe28fe10a3e697bec4a260
Richie029 <codeblings@gmail.com>
2025-09-01 12:13:56 +0530

    Implemented Hero Section along with animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import {gsap} from 'gsap';
 import {ScrollTrigger} from 'gsap/all';
 import Hero from './sections/Hero.jsx';
+import NavBar from './sections/NavBar.jsx';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-      <nav>
-        <img src='/images/nav-logo.svg' className='scale-90' alt='GTA 6 Logo' />
-        <img src="/images/menu.svg" className="w-10" alt="Hamburger menu"/>
-      </nav>
+      <NavBar/>
       <Hero/>
     </main>
   );
---
+++ src/sections/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+      <img src="/images/nav-logo.svg" className="scale-90" alt="GTA 6 Logo" />
+      <img src="/images/menu.svg" className="w-10" alt="Hamburger menu" />
+    </nav>
+  );
+}
+
+export default NavBar
