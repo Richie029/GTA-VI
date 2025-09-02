5ce4e48fbeebf4f6ba485af2cc8c3a9455a0afbb
Richie029 <codeblings@gmail.com>
2025-09-02 11:40:46 +0530

    Complete Build

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import {ScrollTrigger} from 'gsap/all';
 import Hero from './sections/Hero.jsx';
 import NavBar from './sections/NavBar.jsx';
 import FirstVideo from './sections/FirstVideo.jsx';
+import Jason from './sections/Jason.jsx';
+import SecondVideo from './sections/SecondVideo.jsx';
+import Lucia from './sections/Lucia.jsx';
+import Card from './sections/Card.jsx';
+import FinalVideo from './sections/FinalVideo.jsx';
+import Outro from './sections/Outro.jsx';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -12,6 +18,12 @@ const App = () => {
       <NavBar/>
       <Hero/>
       <FirstVideo/>
+      <Jason/>
+      <SecondVideo/>
+      <Lucia/>
+      <Card/>
+      <FinalVideo/>
+      <Outro/>
     </main>
   );
 }
